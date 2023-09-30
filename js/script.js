c59234664eefa9209e17fe5c0c42fa01fa1aa318
aazzam <amandas.azzam@gmail.com>
2023-09-29 16:02:49 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+//Profile information div
+const profileInfo = document.querySelector(".overview");
+const username = "asazzam";
+
+//Fetch API JSON Data
+const getData = async function () {
+  const res = await fetch(`https://api.github.com/users/${username}`);
+  const data = await res.json();
+  console.log(data);
+  userData(data);
+};
+
+getData();
+
+//Fetch and display user information
+const userData = function (data) {
+  const div = document.createElement("div");
+  div.classList.add(".user-info");
+  div.innerHTML = `   <figure>
+      <img alt="user avatar" src=${data.avatar_url} />
+    </figure>
+    <div>
+      <p><strong>Name:</strong> ${data.name}</p>
+      <p><strong>Bio:</strong> ${data.bio}</p>
+      <p><strong>Location:</strong> ${data.location}</p>
+      <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    </div>`;
+  profileInfo.append(div);
+};
